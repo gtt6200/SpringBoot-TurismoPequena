4fc3e899e17ab0f7ca7fe0857001f9e84be0f342
gtt6200 <gtt6200@gmail.com>
2023-07-08 10:29:21 -0600

    Importaciones no utilizadas

--- src/main/java/com/turismopequena/turismopequena/entities/Estado.java
+++ src/main/java/com/turismopequena/turismopequena/entities/Estado.java
@@ -1,8 +1,5 @@
 package com.turismopequena.turismopequena.entities;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
