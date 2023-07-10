ea92455a30e3c26b8a422bb74553d4bbd75999f9
gtt6200 <gtt6200@gmail.com>
2023-07-10 14:22:26 -0600

    Update NotFoundException.java

--- src/main/java/com/turismopequena/turismopequena/exceptions/NotFoundException.java
+++ src/main/java/com/turismopequena/turismopequena/exceptions/NotFoundException.java
@@ -1,5 +1,14 @@
 package com.turismopequena.turismopequena.exceptions;
 
-public class NotFoundException {
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends DetalleException{
+
+	public NotFoundException(String code, String message) {
+		super(code,HttpStatus.NOT_FOUND.value(),message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 
 }
